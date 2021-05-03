dd593513d4a1b4226cf3416add729db075c9d990
abrucm <83360842+abrucm@users.noreply.github.com>
2021-05-03 14:58:41 -0300

    Códigos Arduino
    Hecho en Tinkercad y mejorado en Arduino por Abril Cervera

--- sketch6_apr26d.ino
+++ sketch6_apr26d.ino
@@ -11,12 +11,12 @@ void loop() //se ejecuta un número infinito de veces
 {
   temperatura= -40 + 0.488155 * (analogRead(A0) - 20); //mido la temperatura del sensor, la multiplico por un factor de conversión y la guardo en la variable temperatura
  
-  if (temperatura > 35) //si la temperatura es mayor a 35
+  if (temperatura > 35) //si la temperatura es mayor a 35 grados
   {
     digitalWrite(8, HIGH); //se enciende el pin 8 (LED ROJO)
     digitalWrite(13, LOW); //se apaga el pin 13 (LED VERDE)
   }
-  else //si la temperatura NO es mayor a 35
+  else //si la temperatura NO es mayor a 35 grados
   {
     digitalWrite(8, LOW); //se apaga el pin 8 (LED ROJO)
     digitalWrite(13, HIGH); //se enciende el pin 13 (LED VERDE)
